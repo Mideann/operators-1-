08ae745beaac695c6e15206eed528c65cf60fd44
samuel <mideann@gmail.com>
2024-12-27 15:19:41 +0000

    py operators

---
+++ main.py
@@ -0,0 +1,10 @@
+
+n1=12
+n2=3
+print("Addition:",n1+n2)
+print("Subtraction:",n1-n2)
+print("Multiplication:",n1*n2)
+print("Division:",n1/n2)
+print("Floor division",n1//n2)
+print("Remainder:",n1%n2)
+print("Exponential:",n1**n2)
